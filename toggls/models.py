577ce8a0a26b8053f633a116fa5dd5d9a83d8f10
shierote <taishi2060@gmail.com>
2019-05-05 20:39:49 +0900

    モデルの作成

--- toggls/models.py
+++ toggls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    pid = models.IntegerField()
+    project = models.CharField(max_length=250)
+    project_hex_color = models.CharField(max_length=50)
+
+class Task(models.Model):
+    tid = models.IntegerField()
+    description = models.CharField(max_length=250)
+    start = models.DateTimeField('date started')
+    end = models.DateTimeField('date ended')
+    dur = models.IntegerField(default=0)
+    expected_dur = models.IntegerField(default=0)
+    gap_ratio = models.IntegerField(default=0)
+    pid = models.IntegerField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
